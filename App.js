6bb2d1f04bba5abe482abdef913e1cfdda1ff059
Jesse Weigel <jesseweigel@gmail.com>
2019-10-30 08:58:03 -0400

    feat: add connection to firebase auth

--- App.js
+++ App.js
@@ -7,6 +7,52 @@ import { Ionicons } from '@expo/vector-icons'
 import { Provider as PaperProvider } from 'react-native-paper'
 
 import AppNavigator from './navigation/AppNavigator'
+import * as firebase from 'firebase'
+import {
+  FIREBASE_API_KEY,
+  AUTH_DOMAIN,
+  DATABASE_URL,
+  STORAGE_BUCKET,
+  APPID
+} from 'react-native-dotenv'
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: AUTH_DOMAIN,
+  databaseURL: DATABASE_URL,
+  storageBucket: STORAGE_BUCKET
+}
+
+firebase.initializeApp(firebaseConfig)
+
+// Listen for authentication state to change.
+firebase.auth().onAuthStateChanged(user => {
+  if (user != null) {
+    console.log('We are authenticated now!')
+  }
+
+  // Do other things
+})
+
+async function loginWithFacebook() {
+  const { type, token } = await Facebook.logInWithReadPermissionsAsync(APPID, {
+    permissions: ['public_profile']
+  })
+
+  if (type === 'success') {
+    // Build Firebase credential with the Facebook access token.
+    const credential = firebase.auth.FacebookAuthProvider.credential(token)
+
+    // Sign in with credential from the Facebook user.
+    firebase
+      .auth()
+      .signInWithCredential(credential)
+      .catch(error => {
+        // Handle Errors here.
+      })
+  }
+}
 
 export default function App(props) {
   const [isLoadingComplete, setLoadingComplete] = useState(false)
@@ -24,7 +70,7 @@ export default function App(props) {
       <PaperProvider>
         <View style={styles.container}>
           {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <AppNavigator />
+          <AppNavigator screenProps={{ login: loginWithFacebook }} />
         </View>
       </PaperProvider>
     )
